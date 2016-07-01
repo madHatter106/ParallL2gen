2deeb4fdd574e139b6f2a9cd38c992e5bb564417
madHatter106 <jkomd.283.2112@gmail.com>
2016-07-01 11:18:31 -0400

    mapping out PL2 class

---
+++ ParalleL2.py
@@ -0,0 +1,14 @@
+from subprocess import Popen,STDOUT,DEVNULL
+import sys
+
+Class ParallelL2():
+    '''Manages subprocess spawning'''
+    def __init__(self,pArgs):
+        pass
+
+def Main():
+    '''Function to parse out command line and launch l2gen parallel processing'''
+    pass
+
+if __name__ == '__main__':
+    Main(sys.argv[1:])
